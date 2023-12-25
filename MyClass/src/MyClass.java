1ae46784ccf38b4c8b818ce726594a7edc0a0710
pitomacAhmed <148368787+pitomacAhmed@users.noreply.github.com>
2023-12-25 16:36:25 +0100

    Add files via upload

---
+++ MyClass/src/MyClass.java
@@ -0,0 +1,8 @@
+
+public class MyClass {
+public static void main (String[]args) {
+	Vehicle v1=new Vehicle("Red");
+	
+	System.out.println(v1);
+}
+}
---
+++ MyClass/src/Vehicle.java
@@ -0,0 +1,11 @@
+
+public class Vehicle {
+private String color;
+Vehicle(){
+	this.setColor("Red");
+
+}
+Vehicle (String a){
+	this.setColor(a);
+}
+}
